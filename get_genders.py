2255df195061481a517d643a6ef5a076d22bfe58
matejmecka <matej.plavevski+github@gmail.com>
2022-02-04 23:23:26 +0100

    Add dataset for 25.01.2022

--- get_genders.py
+++ get_genders.py
@@ -5,6 +5,7 @@ This part attempts to guess a persons gender based on the name of the data.
 """
 from genderize import Genderize, GenderizeException
 from transliterate import translit
+from transliterate.exceptions import LanguageDetectionError
 from alive_progress import alive_bar
 
 def guessGenders(df):
@@ -16,7 +17,12 @@ def guessGenders(df):
         for dat in df["Име, име на родител и презиме"]:
             try:
                 name = dat.split(" ")[0]
-                name_data = Genderize().get([translit(name, reversed=True)])
+                try:
+                    name = translit(name, reversed=True)
+                except LanguageDetectionError:
+                    print("Failed Converting name to latin...")
+                    pass
+                name_data = Genderize().get([name])
                 sexes.append(name_data[0]['gender'])
                 probabilities.append(name_data[0]["probability"])
             except GenderizeException:
